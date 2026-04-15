7537859b56ebd8f0620e890581b928f008f1d8af
bassaaaa <white.galaxys2.album2@gmail.com>
2026-04-15 17:58:18 +0900

    publishedがfalseの記事をビルドしないように修正

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -13,6 +13,8 @@ type Props = {
 	params: Promise<{ slug: string }>;
 };
 
+export const dynamicParams = false;
+
 export async function generateStaticParams() {
 	return getAllPosts().map((post) => ({ slug: post.slug }));
 }
